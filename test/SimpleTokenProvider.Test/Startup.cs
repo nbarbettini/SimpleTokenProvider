26aaf23a7c261c765f58e1a8d7435706ed2829ec
William Hallatt <goblincoding@gmail.com>
2016-06-30 21:29:43 +0200

    chore: Upgraded to Core 1.0.0
    Updated projects to MS Core 1.0.0
    Fixed resulting compilation issues
    Upgraded tooling refs (VS2015 Update 3)
    Match latest VS Core WebAPI application
    solution structure and file content

--- test/SimpleTokenProvider.Test/Startup.cs
+++ test/SimpleTokenProvider.Test/Startup.cs
@@ -1,41 +1,44 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace SimpleTokenProvider.Test
 {
-    public partial class Startup
+  public partial class Startup
+  {
+    public Startup(IHostingEnvironment env)
     {
-        public Startup(IHostingEnvironment env)
-        {
-            var builder = new ConfigurationBuilder()
-                .AddJsonFile("appsettings.json", optional: true);
-            Configuration = builder.Build();
-        }
-
-        public IConfigurationRoot Configuration { get; set; }
-
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc();
-        }
-
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
-        {
-            loggerFactory.AddConsole(LogLevel.Debug);
-            loggerFactory.AddDebug();
-
-            ConfigureAuth(app);
-
-            app.UseStaticFiles();
-
-            app.UseMvc();
-        }
+      var builder = new ConfigurationBuilder()
+          .SetBasePath(env.ContentRootPath)
+          .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+          .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+          .AddEnvironmentVariables();
+      Configuration = builder.Build();
     }
+
+    public IConfigurationRoot Configuration { get; set; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddMvc();
+    }
+
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+    {
+      loggerFactory.AddConsole(LogLevel.Debug);
+      loggerFactory.AddDebug();
+
+      ConfigureAuth(app);
+
+      app.UseStaticFiles();
+
+      app.UseMvc();
+    }
+  }
 }
