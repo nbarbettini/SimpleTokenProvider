89ba8fab205aee3b1221cc465d5202684aa0aa07
Miha Markič <miha@rthand.com>
2016-12-02 10:33:21 +0100

    added token refresh functionallity

--- src/SimpleTokenProvider/TokenProviderMiddleware.cs
+++ src/SimpleTokenProvider/TokenProviderMiddleware.cs
@@ -23,18 +23,25 @@ namespace SimpleTokenProvider
     {
         private readonly RequestDelegate _next;
         private readonly TokenProviderOptions _options;
+        private readonly TokenValidationParameters _tokenValidationParameters;
         private readonly ILogger _logger;
         private readonly JsonSerializerSettings _serializerSettings;
 
         public TokenProviderMiddleware(
             RequestDelegate next,
             IOptions<TokenProviderOptions> options,
+            TokenValidationParameters tokenValidationParameters,
             ILoggerFactory loggerFactory)
         {
             _next = next;
             _logger = loggerFactory.CreateLogger<TokenProviderMiddleware>();
+            _tokenValidationParameters = tokenValidationParameters;
 
             _options = options.Value;
+            if(tokenValidationParameters == null)
+            {
+                throw new ArgumentNullException(nameof(tokenValidationParameters));
+            }
             ThrowIfInvalidOptions(_options);
 
             _serializerSettings = new JsonSerializerSettings
@@ -45,8 +52,11 @@ namespace SimpleTokenProvider
 
         public Task Invoke(HttpContext context)
         {
+            // first figure out whether this is a request for a new token or for a refresh
+            bool isCreate = context.Request.Path.Equals(_options.Path, StringComparison.Ordinal);
+            bool isRefresh = !isCreate && context.Request.Path.Equals(_options.RefreshPath, StringComparison.Ordinal);
             // If the request path doesn't match, skip
-            if (!context.Request.Path.Equals(_options.Path, StringComparison.Ordinal))
+            if (!isCreate && !isRefresh)
             {
                 return _next(context);
             }
@@ -59,9 +69,48 @@ namespace SimpleTokenProvider
                 return context.Response.WriteAsync("Bad request.");
             }
 
-            _logger.LogInformation("Handling request: " + context.Request.Path);
+            _logger.LogInformation($"Handling request for {(isCreate ? "create token": "refresh token")}: " + context.Request.Path);
 
-            return GenerateToken(context);
+            if (isCreate)
+            {
+                return GenerateToken(context);
+            }
+            else
+            {
+                return IssueRefreshedToken(context);
+            }
+        }
+
+        private Task IssueRefreshedToken(HttpContext context)
+        {
+            try
+            {
+                // first extract token text from Authorization header
+                string authenticationText = context.Request.Headers["Authorization"].ToString();
+                int firstSpace = authenticationText.IndexOf(" ");
+                string tokenText = authenticationText.Substring(firstSpace + 1);
+
+                var jwtSecurityTokenHandler = new JwtSecurityTokenHandler();
+                SecurityToken originalToken;
+                // validate token using validation parameters
+                var claimsi = jwtSecurityTokenHandler.ValidateToken(tokenText, _tokenValidationParameters, out originalToken);
+                var now = DateTime.UtcNow;
+                // create a new token based on original one 
+                // apply new expiration
+                var jwt = new JwtSecurityToken(
+                 issuer: _options.Issuer,
+                 audience: _options.Audience,
+                 claims: ((JwtSecurityToken)originalToken).Claims,
+                 notBefore: now,
+                 expires: now.Add(_options.Expiration),
+                 signingCredentials: _options.SigningCredentials);
+                return WriteTokenResponse(context, jwt);
+            }
+            catch
+            {
+                context.Response.StatusCode = 400;
+                return context.Response.WriteAsync("Bad request or invalid token.");
+            }
         }
 
         private async Task GenerateToken(HttpContext context)
@@ -96,6 +145,11 @@ namespace SimpleTokenProvider
                 notBefore: now,
                 expires: now.Add(_options.Expiration),
                 signingCredentials: _options.SigningCredentials);
+            await WriteTokenResponse(context, jwt);
+        }
+
+        private async Task WriteTokenResponse(HttpContext context, JwtSecurityToken jwt)
+        {
             var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
 
             var response = new
