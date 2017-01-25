c013de70e9704ca3f5ba027944f18f4d697de261
Nate Barbettini <nathanaelb@gmail.com>
2017-01-24 16:02:34 -0800

    Upgrade packages due to security advisory

--- src/SimpleTokenProvider/TokenProviderMiddleware.cs
+++ src/SimpleTokenProvider/TokenProviderMiddleware.cs
@@ -8,7 +8,6 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using Microsoft.IdentityModel.Tokens;
 using Newtonsoft.Json;
 
 namespace SimpleTokenProvider
