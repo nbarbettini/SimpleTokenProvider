d74333065f8a33e7e58d1f05fcd21abe96766fb9
Nate Barbettini <nathanaelb@gmail.com>
2017-01-24 16:10:13 -0800

    Example of getting user information in controller

---
+++ test/SimpleTokenProvider.Test/Controllers/MeController.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleTokenProvider.Test.Controllers
+{
+    [Route("api/[controller]")]
+    [Authorize]
+    public class MeController : Controller
+    {
+        public string Get()
+        {
+            // The JWT "sub" claim is automatically mapped to ClaimTypes.NameIdentifier
+            // by the UseJwtBearerAuthentication middleware
+            var username = HttpContext.User.Claims.First(c => c.Type == ClaimTypes.NameIdentifier).Value;
+
+            return $"Hello {username}!";
+        }
+    }
+}
